f1ba79b82ee1d85cb8596b3b939afdec0b277811
clementpl <clement22860@gmail.com>
2018-12-08 18:28:17 +0100

    refactor mongo URL

--- src/_core/Mongo/Mongo.ts
+++ src/_core/Mongo/Mongo.ts
@@ -8,16 +8,20 @@ export interface MongoConfig {
 }
 
 export class Mongo {
+  public static URL: string;
+
   public static async connect(conf: MongoConfig) {
-    const URL = `mongodb://${conf.host}:${conf.port}/${conf.database}`;
+    // Set mongo url
+    Mongo.URL = `mongodb://${conf.host}:${conf.port}/${conf.database}`;
+    // connect
     try {
       await mongoose.connect(
-        URL,
+        Mongo.URL,
         { useNewUrlParser: true }
       );
-      logger.info(`[MONGODB] Connection successful: ${URL}`);
+      logger.info(`[MONGODB] Connection successful: ${Mongo.URL}`);
     } catch (error) {
-      logger.error(new Error(`[MONGODB] Connection error: ${URL}`));
+      logger.error(new Error(`[MONGODB] Connection error: ${Mongo.URL}`));
       throw error;
     }
   }
@@ -26,7 +30,7 @@ export class Mongo {
     try {
       await mongoose.connection.close();
     } catch (error) {
-      logger.error(new Error(`[MONGODB] Close connection error: ${URL}`));
+      logger.error(new Error(`[MONGODB] Close connection error: ${Mongo.URL}`));
       throw error;
     }
   }
