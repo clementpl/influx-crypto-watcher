d06aadf5091e971092b299f49c94091ef154651d
clementpl <clement22860@gmail.com>
2019-05-18 15:12:41 +0200

    Add backup influxdb

--- docker/influx/init-test.sh
+++ docker/influx/init-test.sh
@@ -8,7 +8,7 @@ curl -XPOST "http://127.0.0.1:8086/query" --data-urlencode "q=DROP DATABASE \"te
 docker exec influxdb influxd restore -portable -db test_crypto_series /testset
 
 # Make backup
-# docker run influxdb influxd backup -portable -db test_crypto_series -end 2018-03-15T00:00:00Z -start 2018-02-15T00:00:00Z ./testset
+# docker exec influxdb influxd backup -portable -db test_crypto_series -end 2018-03-15T00:00:00Z -start 2018-02-15T00:00:00Z ./testset
 # docker cp influxdb:/testset .
 
 # COPY DATA INFLUX (FROM ONE DB TO ANOTHER)
@@ -16,6 +16,4 @@ docker exec influxdb influxd restore -portable -db test_crypto_series /testset
 # SELECT * INTO test_crypto_series..OHLC_FILLED FROM dev_crypto_series..OHLC_FILLED group by *
 
 # package.json suggest
-# "backup-influx": "docker run influxdb influxd backup -portable ./backup/`date +\"%m_%d_%Y\"`",
 # "restore-influx": "docker run influxdb influxd restore -portable",
-
